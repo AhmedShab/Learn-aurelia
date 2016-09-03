608db2061810b60fd83335c5379be5537ddc49d3
AhmedShab <ahmed.vuw@gmail.com>
2016-09-03 13:24:15 +1200

    getting started with todos app

---
+++ todo/scripts/app-bundle.js
@@ -0,0 +1,120 @@
+define('app',['exports', './todo'], function (exports, _todo) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.App = undefined;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var App = exports.App = function () {
+    function App() {
+      _classCallCheck(this, App);
+
+      this.heading = 'Todos';
+      this.todos = [];
+      this.todoDescription = '';
+    }
+
+    App.prototype.addTodo = function addTodo() {
+      if (this.discription) {
+        this.todos.push(new _todo.Todo(this.todoDescription));
+        this.todoDescription = '';
+      }
+    };
+
+    App.prototype.removeTodo = function removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    };
+
+    return App;
+  }();
+});
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+
+  var _environment2 = _interopRequireDefault(_environment);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  Promise.config({
+    warnings: {
+      wForgottenReturn: false
+    }
+  });
+
+  function configure(aurelia) {
+    aurelia.use.standardConfiguration().feature('resources');
+
+    if (_environment2.default.debug) {
+      aurelia.use.developmentLogging();
+    }
+
+    if (_environment2.default.testing) {
+      aurelia.use.plugin('aurelia-testing');
+    }
+
+    aurelia.start().then(function () {
+      return aurelia.setRoot();
+    });
+  }
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('todo',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var Todo = exports.Todo = function Todo(discription) {
+    _classCallCheck(this, Todo);
+
+    this.discription = discription;
+    this.done = false;
+  };
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <h1>${heading}</h1>\n\n  <form submit.trigger=\"addTodo()\">\n    <input type=\"text\" name=\"name\" value.bind=\"todoDescription\">\n    <button type=\"button\" name=\"button\">Add Todo</button>\n  </form>\n\n</template>\n"; });
+//# sourceMappingURL=app-bundle.js.map
---
+++ todo/src/todo.js
@@ -0,0 +1,6 @@
+export class Todo {
+  constructor(discription) {
+    this.discription = discription;
+    this.done = false;
+  }
+}
